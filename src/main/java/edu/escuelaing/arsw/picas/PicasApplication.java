95b4be9cd566e8c1819503918aa3ce597b699ff5
Daniela Pachon <laura.pachon-c@mail.escuelaing.edu.co>
2021-06-21 11:46:45 -0500

    APP

--- src/main/java/edu/escuelaing/arsw/picas/PicasApplication.java
+++ src/main/java/edu/escuelaing/arsw/picas/PicasApplication.java
@@ -3,11 +3,23 @@ package edu.escuelaing.arsw.picas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class PicasApplication{
-
 	public static void main(String[] args) {
-		SpringApplication.run(PicasApplication.class, args);
+		int port = getPort();
+		SpringApplication app = new SpringApplication(PicasApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", port));
+		app.run(args);
+		//SpringApplication.run(PicasApplication.class, args);
+	}
+	static int getPort() {
+		if (System.getenv("PORT") != null) {
+			return Integer.parseInt(System.getenv("PORT"));
+		}
+		return 5000; //returns default port if heroku-port isn't set(i.e. on localhost)
 	}
 }
 
